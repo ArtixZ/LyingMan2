579e4f15b54af1d698a7dbadc9ca2c24270c696b
ArtixZ <artixzhu@gmail.com>
2017-05-06 02:28:34 -0400

    finish the SPA

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,121 @@
 import React, { Component } from 'react';
 
+
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			peopleCount: null
+		}
+
+		this.dict = {
+			6: ['预言家', '女巫', '猎人', '白痴', '狼人', '狼人', '平民', '平民', '平民', '平民', '平民', '平民'],
+			7: ['预言家', '女巫', '猎人', '白痴', '狼人', '狼人', '平民', '平民', '平民', '平民', '平民', '平民', '平民', '平民'],
+			8: ['预言家', '女巫', '猎人', '白痴', '狼人', '狼人', '平民', '平民', '平民', '平民', '平民', '平民', '平民', '平民', '狼人', '守卫']
+		}
+
+		this.onInputChange = this.onInputChange.bind(this);
+		this.renderTable = this.renderTable.bind(this);
+		this.renderRows = this.renderRows.bind(this);
+		this.shuffle = this.shuffle.bind(this);
+	}
+
+	onInputChange(event) {
+		this.setState({
+			peopleCount: parseInt(event.target.value)
+		})
+	}
+
+	shuffle(array) {
+	  let currentIndex = array.length;
+	  let temporaryValue;
+	  let randomIndex;
+
+	  // While there remain elements to shuffle...
+	  while (0 !== currentIndex) {
+
+	    // Pick a remaining element...
+	    randomIndex = Math.floor(Math.random() * currentIndex);
+	    currentIndex -= 1;
+
+	    // And swap it with the current element.
+	    temporaryValue = array[currentIndex];
+	    array[currentIndex] = array[randomIndex];
+	    array[randomIndex] = temporaryValue;
+	  }
+
+	  return array;
+	}
+
+	renderRows(count, shuffleRoles1, shuffleRoles2) {
+		let res=[];
+		let i;
+
+		for (i=0; i<count; i++) {
+			res.push(
+				<tr>
+					<td>{i}</td>
+					<td>{shuffleRoles1[i]}</td>
+					<td>{shuffleRoles2[i]}</td>
+				</tr>
+				)
+		}
+		return res;
+	}
+
+	renderTable() {
+
+		let resTable = null;
+		const count = this.state.peopleCount;
+		let flag = true;
+
+		if (typeof(count) === 'number' && count > 5 && count < 9) {
+			const roles = this.dict[count];
+			let shuffleRoles1;
+			let shuffleRoles2;
+			while(flag) {
+				const shuffleRoles = this.shuffle(roles);
+				shuffleRoles1 = shuffleRoles.slice(0, count);
+				shuffleRoles2 = shuffleRoles.slice(count);
+				let i;
+				for (i=0;i<count;i++) {
+					if(shuffleRoles1[i] === '狼人' && shuffleRoles2[i] === '狼人') flag = true;
+					if((shuffleRoles1[i] === '狼人' && shuffleRoles2[i] === '预言家') || (shuffleRoles2[i] === '狼人' && shuffleRoles1[i] === '预言家')) flag = true;
+				}
+				if ( i===count) {
+					flag = false;
+				}
+			}
+			resTable = this.renderRows(count, shuffleRoles1, shuffleRoles2);
+			
+		}
+		    		
+		return resTable;
+	}
+
+	render() {
+	return (
+		<div>
+	        <input
+	          placeholder="请输入参与人数"
+	          className="form-control"
+	          value={this.state.term}
+	          onChange={this.onInputChange} />
+		        
+		    <table className="table table-striped">
+		    	<thead>
+			    	<tr>
+			    		<th>编号</th>
+			    		<th>职业1</th>
+			    		<th>职业2</th>
+			    	</tr>
+			    </thead>
+			    <tbody>
+		    		{this.renderTable()}
+			    </tbody>
+		    </table>
+		    
+		</div>
+	);
+	}
 }
